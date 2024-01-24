61753c42f92ecfa5c112fce90a6f8246458633c1
Piotr Wendt <wendtpiotr333@gmail.com>
2024-01-24 15:51:24 +0100

    ops: create player object, feat: make Movement and ActionController script

---
+++ Assets/Scripts/ActionController.cs
@@ -0,0 +1,178 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionController : MonoBehaviour
+{
+    [Header("Action Controller")]
+    [Space]
+    [Header("References")]
+    [SerializeField] Rigidbody rigidbody;
+    [SerializeField] Movement movement;
+    [SerializeField] Animator animator;
+    [SerializeField] LayerMask ballLayer;
+    [Space]
+    [Header("Keycodes")]
+    [SerializeField] KeyCode kickKey = KeyCode.F;
+    [SerializeField] float kickForce = 10f;
+    [SerializeField] float kickCooldown = 0.25f;
+    [SerializeField] float kickMultiplier = 0.65f;
+    [SerializeField] float actualKickForce;
+    [HideInInspector] public bool isKicking;
+    [HideInInspector] bool canShowKick;
+    [Space]
+    [SerializeField] KeyCode tackleKey = KeyCode.G;
+    [SerializeField] float tackleSpeed = 15f;
+    [SerializeField] float tackleCooldown = 1f;
+    [SerializeField] float tackleMultiplier = 1f;
+    [SerializeField] float actualTackleSpeed;
+    [HideInInspector] public bool isTackling;
+    [HideInInspector] bool canShowTackle;
+    [HideInInspector] Vector3 tackleDirection;
+    [Space]
+    [SerializeField] KeyCode diveKey = KeyCode.B;
+    [SerializeField] float diveForce = 5f;
+    [SerializeField] float diveCooldown = 1f;
+    [SerializeField] float diveMultiplier = 0.65f;
+    [SerializeField] float actualDiveForce;
+    [HideInInspector] public bool isDiving;
+    [HideInInspector] bool canShowDive;
+    [HideInInspector] Vector3 diveDirection;
+
+    // 
+    // VISUAL DEBUGGING
+    // 
+
+    void debugKickGizmos(){
+        Gizmos.color = Color.blue;
+        Vector3 direction = this.transform.position + movement.lastDirection - Vector3.up * 0.5f;
+        Gizmos.DrawSphere(direction, 0.75f);
+    }
+
+    public void OnDrawGizmosSelected(){
+        debugKickGizmos();
+    }
+
+
+    void Update(){
+        animator.SetBool("isRunning", movement.direction != Vector3.zero);
+
+        // 
+        // Kicking
+        if(Input.GetKeyDown(kickKey) && !isKicking){
+            isKicking = true;
+            StartCoroutine("kickForceIncrementation");
+        }
+        if(Input.GetKeyUp(kickKey) && isKicking){
+            movement.canMove = false;
+            canShowKick = true;
+            StopAllCoroutines();
+            Invoke("handleKick", kickCooldown / 2);
+            Invoke("stopKicking", kickCooldown / 2);
+        }
+
+        // 
+        // Tackling
+        if(Input.GetKeyDown(tackleKey) && !isTackling){
+            isTackling = true;
+            StartCoroutine("tackleSpeedIncrementation");
+        }
+        if(Input.GetKeyUp(tackleKey) && isTackling){
+            movement.canMove = false;
+            canShowTackle = true;
+            tackleDirection = movement.lastDirection;
+            StopAllCoroutines();
+            Invoke("stopTackling", tackleCooldown);
+        }
+        if(canShowTackle){
+            rigidbody.AddForce(tackleDirection * actualTackleSpeed, ForceMode.Force);
+        }
+
+
+        // 
+        // Diving
+        if(Input.GetKeyDown(diveKey) && !isDiving){
+            isDiving = true;
+            StartCoroutine("diveForceIncrementation");
+        }
+        if(Input.GetKeyUp(diveKey) && isDiving){
+            movement.canMove = false;
+            canShowDive = true;
+            diveDirection = movement.lastDirection;
+            StopAllCoroutines();
+            Invoke("stopDiving", diveCooldown);
+        }
+        if(canShowDive){
+            rigidbody.AddForce(diveDirection * actualDiveForce + Vector3.up * actualDiveForce / 5, ForceMode.Force);
+        }
+
+
+
+        // 
+        // Clamping values
+        actualKickForce = Mathf.Clamp(actualKickForce, 0, kickForce);
+        actualTackleSpeed = Mathf.Clamp(actualTackleSpeed, 0, tackleSpeed);
+        actualDiveForce = Mathf.Clamp(actualDiveForce, 0, diveForce);
+
+        //
+        // Animator States
+        animator.SetBool("isKicking", canShowKick);
+        animator.SetBool("isTackling", canShowTackle);
+        animator.SetBool("isDiving", canShowDive);
+    }
+
+
+    // 
+    // Kicking
+    void stopKicking(){
+        isKicking = false;
+        canShowKick = false;
+        movement.canMove = true;
+    }
+    void handleKick(){
+        Collider[] colliders = Physics.OverlapSphere(this.transform.position + movement.lastDirection - Vector3.up * 0.5f, 0.65f, ballLayer);
+        for(int i = 0; i < colliders.Length; i++){
+            Rigidbody ball = colliders[i].transform.gameObject.GetComponent<Rigidbody>();
+            ball.AddForce(movement.lastDirection * actualKickForce, ForceMode.Impulse);
+        }
+        actualKickForce = 0;
+    }
+    IEnumerator kickForceIncrementation(){
+        while(actualKickForce < kickForce){
+            actualKickForce += kickMultiplier;
+            yield return new WaitForSecondsRealtime(0.05f);
+        }
+    }
+
+
+    // 
+    // Tackling
+    void stopTackling(){
+        canShowTackle = false;
+        isTackling = false;
+        movement.canMove = true;
+        actualTackleSpeed = 0;
+    }
+    IEnumerator tackleSpeedIncrementation(){
+        while(actualTackleSpeed < tackleSpeed){
+            actualTackleSpeed += tackleMultiplier;
+            yield return new WaitForSecondsRealtime(0.05f);
+        }
+    }
+
+
+    // 
+    // Diving
+    void stopDiving(){
+        canShowDive = false;
+        isDiving = false;
+        movement.canMove = true;
+        actualDiveForce = 0;
+    }
+    IEnumerator diveForceIncrementation(){
+        while(actualDiveForce < diveForce){
+            actualDiveForce += diveMultiplier;
+            yield return new WaitForSecondsRealtime(0.05f);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Header("Movement")]
+    [Space]
+    [Header("References")]
+    [SerializeField] Rigidbody rigidbody;
+    [Space]
+    [Header("Variables")]
+    [SerializeField] public bool canMove = true;
+    [SerializeField] float speed = 1000f;
+    [SerializeField] float maxSpeed = 5f;
+    [SerializeField] float stopSmoothness = 5f;
+    [SerializeField] float rotationSpeedMultiplier = 10f;
+    [HideInInspector] float directionX; 
+    [HideInInspector] float directionZ;
+    [HideInInspector] public Vector3 direction;
+    [HideInInspector] public Vector3 lastDirection;
+
+
+    // 
+    // VISUAL DEBBUGING
+    // 
+
+    void debugDirectionGizmos(){
+        Gizmos.color = Color.green;
+        Vector3 direction = new Vector3(transform.position.x + directionX * 2f, transform.position.y, transform.position.z + directionZ * 2f);
+        Gizmos.DrawLine(transform.position, direction);
+    }
+
+    public void OnDrawGizmosSelected(){
+        debugDirectionGizmos();
+    }
+
+
+    public void Update(){
+        directionX = Input.GetAxisRaw("Horizontal");
+        directionZ = Input.GetAxisRaw("Vertical");
+        direction = new Vector3(directionX, 0f, directionZ).normalized;
+    }
+
+    public void FixedUpdate(){
+        move();
+        rotateTowardDirection();
+    }
+
+    // 
+    // Movement
+    // 
+    
+    void move(){
+        if(direction == Vector3.zero || !canMove){
+            rigidbody.velocity = Vector3.Slerp(rigidbody.velocity, new Vector3(0,rigidbody.velocity.y, 0), Time.fixedDeltaTime * stopSmoothness);
+        }else{
+            rigidbody.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.Force); 
+        }
+        rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxSpeed);
+    }
+
+    // 
+    // Rotation
+    // 
+
+    void rotateTowardDirection(){
+        if (direction != Vector3.zero && canMove)
+        {
+            lastDirection = direction;
+            Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeedMultiplier * Time.fixedDeltaTime);
+        }
+        else
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(lastDirection, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeedMultiplier * Time.fixedDeltaTime);
+        }
+    }
+
+}
