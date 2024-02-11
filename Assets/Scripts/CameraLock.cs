b3822fc83328f9e7848dbe57302fd1509a8047c1
Piotr Wendt <wendtpiotr333@gmail.com>
2024-02-11 15:48:58 +0100

    feat: reeengineer camera lock to follow player and ball at the same time, ops: make better scene, install skybox packages

--- Assets/Scripts/CameraLock.cs
+++ Assets/Scripts/CameraLock.cs
@@ -6,11 +6,19 @@ public class CameraLock : MonoBehaviour
 {
     [Header("Camera Lock")]
     [Space]
+    [SerializeField] Camera camera;
     [SerializeField] Transform ball;
+    [SerializeField] Transform player;
     [SerializeField] float followSpeed;
 
     void Update(){
-        Quaternion lookOnRotation = Quaternion.LookRotation(ball.transform.position - this.transform.position);
-        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookOnRotation, followSpeed * Time.deltaTime);
+        Quaternion lookOnRotationBall = Quaternion.LookRotation(ball.transform.position - this.transform.position);
+        Quaternion lookOnRotationPlayer = Quaternion.LookRotation(player.transform.position - this.transform.position);
+
+        float distanceToBall = Vector3.Distance(player.transform.position, ball.transform.position);
+        Quaternion crossRotation = Quaternion.Slerp(lookOnRotationPlayer, lookOnRotationBall,  0.5f);
+
+        this.transform.position = Vector3.Slerp(this.transform.position, new Vector3(player.transform.position.x + (distanceToBall * 0.5f), this.transform.position.y, this.transform.position.z), followSpeed * Time.deltaTime);
+        this.transform.rotation = Quaternion.Lerp(this.transform.rotation, crossRotation, followSpeed * Time.deltaTime);
     }
 }
